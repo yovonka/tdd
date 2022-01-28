c3d7a2482b2939fe304ea3bf8b8fd4aa1e456625
yovonka <iovonkagawel@gmail,com>
2022-01-28 10:04:55 +0100

    create basic class

---
+++ tdd/src/test/java/shop/BasketTest.java
@@ -0,0 +1,9 @@
+package shop;
+
+import static org.junit.Assert.*;
+
+public class BasketTest {
+
+
+
+}
---
+++ tdd/src/test/java/shop/ItemTest.java
@@ -0,0 +1,7 @@
+package shop;
+
+import static org.junit.Assert.*;
+
+public class ItemTest {
+
+}
